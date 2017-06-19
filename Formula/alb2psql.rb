563ab5126b922e7b936ef72548976be11d6bcefa
Nick Mitchinson <nrmitchi@gmail.com>
2017-06-19 15:33:57 -0500

    Add Formula

---
+++ Formula/alb2psql.rb
@@ -0,0 +1,24 @@
+class Alb2Psql < Formula
+  desc "Tool to fetch ALB request logs, and load into a local PostgreSQL instance"
+  homepage "https://github.com/nrmitchi/alb2psql"
+  url "https://github.com/ahmetb/nrmitchi/alb2psql/v0.0.1.tar.gz"
+  sha256 "2c7da89577182db466e60f8c3d6b2f35e48d2e876435f395c10e2e49facbef7f"
+  head "https://github.com/nrmitchi/alb2psql.git", :branch => "master"
+  bottle :unneeded
+
+  # option "with-short-names", "link as \"kctx\" and \"kns\" instead"
+
+  def install
+    bin.install "alb2psql" => "alb2psql"
+    # include.install "utils.bash"
+
+    # bash_completion.install "completion/kubectx.bash" => "kubectx"
+    # bash_completion.install "completion/kubens.bash" => "kubens"
+    # zsh_completion.install "completion/kubectx.zsh" => "_kubectx"
+    # zsh_completion.install "completion/kubens.zsh" => "_kubens"
+  end
+
+  test do
+    system "which", "alb2psql"
+  end
+end
